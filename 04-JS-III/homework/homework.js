04e051c71d2af0cc19b1b5a1524e35fced229e95
OctaRod <octaviotrodriguez@gmail.com>
2022-11-15 13:53:44 -0300

    Nada

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -67,7 +67,7 @@ function agregarNumeros(numeros) {
 function promedioResultadosTest(resultadosTest) {
   // "resultadosTest" debe ser una matriz de enteros (int/integers)
   // Itera (en un bucle) los elementos del array, calcula y devuelve el promedio de puntajes
-  // Tu código:
+  // Tu código  :
 }
 
 
